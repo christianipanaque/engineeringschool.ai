6e840c0bd389a90e9b4bfe7457b2b8d8fd2263ad
christian <christian@christians-MacBook-Pro.local>
2023-08-05 07:51:13 -0700

    the start of engineringschool.ai

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('cta-button').addEventListener('click', function (e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    if (email) {
+        // Save email or send to server
+    }
+});
